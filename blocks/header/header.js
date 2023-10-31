2d0065fa59398edb8d94684874a3c970b72ad043
Vinnie <vinnies@ciandt.com>
2023-10-31 16:31:52 +0800

    initial code

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -113,6 +113,9 @@ export default async function decorate(block) {
     });
 
     const navSections = nav.querySelector('.nav-sections');
+    navSections.children[0].classList.add('nav-anchor');
+    navSections.children[1].classList.add('nav-pathlink');
+
     if (navSections) {
       navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
         if (navSection.querySelector('ul'))
