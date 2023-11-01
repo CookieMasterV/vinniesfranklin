c1eff9ef885aa9c96091c4e720c2a8151bdb6a76
Vinnie <vinnies@ciandt.com>
2023-11-01 13:46:38 +0800

    change header

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -15,15 +15,34 @@ window.addEventListener('resize', () => setHeight());
 
 export default function decorate(block) {
   const classes = ['logo', 'bannerbg', 'info', 'ref'];
+  const currentURL = window.location.href;
+  const languageRegex = /\/(en)\//;
+  const regex = /^\/$/;
+  const isMatch = regex.test(window.location.pathname);
+  console.log(isMatch);
+  const match = currentURL.match(languageRegex);
+  console.log(window.location.pathname);
   [...classes].forEach((c, i) => {
     const section = block.children[i];
     if (section) {
       section.classList.add(`hero-${c}`);
       if (i === 0) {
         const logo = document.createElement('a');
-        logo.href = '/';
-        if (section.querySelector('picture')) logo.innerHTML = section.innerHTML;
-        else logo.innerHTML = '<img loading="lazy" alt="Logo" width="128" height="54" src="/imgs/white-logo.svg">';
+        const pic = section.querySelector('picture');
+        if (match) {
+          logo.href = '/en/';
+        } else {
+          logo.href = '/';
+        }
+        if (pic) {
+          logo.innerHTML = section.innerHTML
+        } else {
+          if(isMatch) {
+            logo.innerHTML = '<img loading="lazy" alt="Logo" width="128" height="54" src="/imgs/white-logo.svg">';
+          } else {
+          logo.innerHTML = '<img loading="lazy" alt="Logo" width="128" height="54" src="/imgs/logo-blue.svg">';
+          }
+        }
         logo.setAttribute('sc:linkname', `global header|navigation|${messages.home}`);
         section.textContent = '';
         section.append(logo);
